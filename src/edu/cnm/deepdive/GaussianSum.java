8d90886c6c89bd9d1c2c2ec2490b5b89ab5935f2
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-05-21 10:32:27 -0600

    scanner changes and message added for invalid inputs

--- src/edu/cnm/deepdive/GaussianSum.java
+++ src/edu/cnm/deepdive/GaussianSum.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class GaussianSum {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int upperLimit;
-        
-        upperLimit = -1;
-        long sum;
-        
-        while (upperLimit < 1 || upperLimit > 1000000) {
-            System.out.println(" Please enter a value between 1 and 1000000.");
-            upperLimit = input.nextInt();
-        }
-        sum = ((long) upperLimit * (upperLimit + 1)) / 2;
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int upperLimit;
 
-        System.out.println(" The sum of 1 to " + upperLimit + " is " + sum + "." + " Great job kiddo!");
+    upperLimit = -1;
+    long sum;
+
+    while (upperLimit < 1 || upperLimit > 1000000) {
+      System.out.println(" Please enter a value between 1 and 1000000.");
+      while (!input.hasNextInt()) {
+        input.next();
+        System.out.println("Invalid input. Please enter a value between 1 and 1000000. ");
+      }
+      upperLimit = input.nextInt();
     }
+    sum = ((long) upperLimit * (upperLimit + 1)) / 2;
+
+    System.out.println(" The sum of 1 to " + upperLimit + " is " + sum + "." + " Great job kiddo!");
+  }
 }
