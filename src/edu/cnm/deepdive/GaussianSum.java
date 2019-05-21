dff1163e8346fa962d79f91881a7c7eaa26e03ab
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-05-21 10:02:14 -0600

    name change for gaussian sum and fixed code for java

---
+++ src/edu/cnm/deepdive/GaussianSum.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive;
+
+import java.util.Scanner;
+
+public class GaussianSum {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int upperLimit;
+        
+        upperLimit = -1;
+        long sum;
+        
+        while (upperLimit < 1 || upperLimit > 1000000) {
+            System.out.println(" Please enter a value between 1 and 1000000.");
+            upperLimit = input.nextInt();
+        }
+        sum = ((long) upperLimit * (upperLimit + 1)) / 2;
+
+        System.out.println(" The sum of 1 to " + upperLimit + " is " + sum + "." + " Great job kiddo!");
+    }
+}
